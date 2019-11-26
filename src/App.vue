00d9e017763023318a946267ac7665dbc3c31725
Brunnhild <10175101259@stu.ecnu.edu.cn>
2019-11-26 20:34:52 +0800

    added some test code for PIXI

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -25,7 +21,7 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -38,24 +34,79 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <!--      <HelloWorld />-->
+      <div id="canvas"></div>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import { Vue, Component } from 'vue-property-decorator'
+import * as PIXI from 'pixi.js'
+import img from './assets/ball.jpg'
 
-export default Vue.extend({
-  name: 'App',
-
+@Component({
   components: {
-    HelloWorld,
+    HelloWorld
   },
+  mounted() {
+    let app: PIXI.Application = new PIXI.Application({
+      width: 256,
+      height: 256
+    })
 
-  data: () => ({
-    //
-  }),
-});
+    app.renderer.backgroundColor = 0x061639
+
+    app.loader.add(img).load(setup)
+
+    function setup() {
+      console.log('set up')
+
+      let sprite: PIXI.Sprite = new PIXI.Sprite(
+        app.loader.resources[img].texture
+      )
+
+      let rectangle = new PIXI.Graphics()
+      rectangle.beginFill(0xffffff)
+      rectangle.drawCircle(100, 100, 50)
+      rectangle.endFill()
+
+      let style = new PIXI.TextStyle({
+        fontFamily: 'Arial',
+        fontSize: 36,
+        fill: 'white',
+        stroke: '#ff3300',
+        strokeThickness: 4,
+        dropShadow: true,
+        dropShadowColor: '#000000',
+        dropShadowBlur: 4,
+        dropShadowAngle: Math.PI / 6,
+        dropShadowDistance: 6
+      })
+      let message = new PIXI.Text('Hello World', style)
+
+      app.stage.addChild(sprite)
+      app.stage.addChild(rectangle)
+      app.stage.addChild(message)
+
+      sprite.x = 100
+      sprite.y = 100
+      sprite.width = 100
+      sprite.rotation = 0.5
+
+      app.ticker.add(delta => gameLoop(delta, sprite))
+    }
+
+    function gameLoop(delta: number, sprite: PIXI.Sprite) {
+      sprite.rotation += 1
+    }
+
+    let element: null | HTMLElement = document.getElementById('canvas')
+    if (element) {
+      element.appendChild(app.view)
+    }
+  }
+})
+export default class App extends Vue {}
 </script>
